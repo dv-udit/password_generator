27f812cf76a8054dd09bb6b9de17c9568167d694
Udit Sharma <udit.sharma@dataverze.ai>
2023-07-31 11:58:10 +0530

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,27 +1,24 @@
 import setuptools
-# from version import __version__
-import version
-# with open("README.md", "r") as fh:
-#     long_description = fh.read()
+from pkgUdit.version import __version__
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setuptools.setup(
-    name="pkgUdit",                     # This is the name of the package
-    version= version,                        # The current release version
-    author="Udit Sharma",                     # Full name of the author
-    description="The is a sample package",
-    zip_safe=False,
+    name="pkgUdit",
+    version=__version__,
+    author="Udit Sharma",
+    description="A sample package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
     package_data={'pkgUdit': ['data/*.csv']},
     include_package_data=True,
-    # long_description=long_description,      # Long description read from the the readme file
-    long_description_content_type="text/markdown",
-    packages=setuptools.find_packages(),    # List of all python modules to be installed
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ],                                      # Information to filter the project on PyPi website
-    python_requires='>=3.6',                # Minimum version requirement of the package
-    py_modules=["pkgUdit"],             # Name of the python package
-    package_dir={'':'.'},     # Directory of the source code of the package
-    install_requires=[]                     # Install other dependencies if any
+    ],
+    python_requires='>=3.6',
+    install_requires=[]
 )
