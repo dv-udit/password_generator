b14ac8e03be47aaee64b6db8f4f12152faff2777
Udit Sharma <udit.sharma@dataverze.ai>
2023-07-31 11:59:09 +0530

    misc

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 import setuptools
 from pkgUdit.version import __version__
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+# with open("README.md", "r") as fh:
+#     long_description = fh.read()
 
 setuptools.setup(
     name="pkgUdit",
     version=__version__,
     author="Udit Sharma",
     description="A sample package",
-    long_description=long_description,
+    # long_description=long_description,
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
     package_data={'pkgUdit': ['data/*.csv']},
